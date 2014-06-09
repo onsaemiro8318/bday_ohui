83aabbfc053140358ff62497345f9ae67af0f7bf
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-06-10 01:39:12 +0900

    turbolinks 제거

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-// require turbolinks
 //= require jquery.bpopup.min
 //= require jquery.customSelect.min
 //= require icheck
--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require pc
 //= require jquery.bpopup.min
 //= require jquery.customSelect.min
