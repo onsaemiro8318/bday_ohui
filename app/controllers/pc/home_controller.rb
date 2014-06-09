ec5721cb4f7e841873670952d8dac5547ae6530f
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-06-09 17:51:55 +0900

    이미지 링크 주소 변경

--- app/controllers/pc/home_controller.rb
+++ app/controllers/pc/home_controller.rb
@@ -2,6 +2,6 @@ class Pc::HomeController < ApplicationController
   def index
     @user = User.new
     @code = '<a href="https://birthday.su-m37.co.kr/?s=blog">
-    <img src="http://bday.mnv.kr/blog_730.jpg"/></a>'
+    <img src="http://birthday.su-m37.co.kr/blog_730.jpg"/></a>'
   end
 end
