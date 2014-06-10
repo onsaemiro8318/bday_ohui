2b531965b15d102926a25d35f810623ce55f99f8
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-06-10 13:59:22 +0900

    logger

--- app/controllers/pc/home_controller.rb
+++ app/controllers/pc/home_controller.rb
@@ -1,5 +1,6 @@
 class Pc::HomeController < ApplicationController
   def index
+    Rails.logger.info "@@@env"+Rails.env
     @user = User.new
     @code = '<a href="https://birthday.su-m37.co.kr/?s=blog">
     <img src="http://birthday.su-m37.co.kr/blog_730.jpg"/></a>'
