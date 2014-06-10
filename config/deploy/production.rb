2b71958cc4975de3de60fe436fd0f9cff10c3662
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-06-10 12:06:46 +0900

    setting for production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,24 +1,6 @@
 server 'm5.mnv.kr', user: 'onesup', roles: %w{web app db}# , my_property: :my_value
-
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
 set :ssh_options, {
   keys: %w(/Users/daul/.ssh/ids/m5.mnv.kr/deployer/id_rsa),
   forward_agent: false
-  # use_agent: false
-  # auth_methods: %w(password)
 }
-# and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
-# setting per server overrides global ssh_options
+set :rails_env, :production
