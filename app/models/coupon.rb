02bcf432671b35f0c2e1038e265d898864b893bd
onesup <onesup@test.net>
2014-07-28 17:42:34 +0900

    쿠폰 처리

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -5,12 +5,33 @@ class Coupon < ActiveRecord::Base
   scope :used, -> { where status: 'used' }
   scope :not_used, -> { where status: 'not_used' }
   
+  def self.log_file
+    file = File.new("ohui_production_07281420.log")
+    lines = file.readlines
+    greps = lines.grep(/Parameters: {"contraints"=>{"code"=>/)
+    greps
+    coupons = []
+    greps.each_with_index do |grep, i|
+      hash = eval grep.partition("ters: ").last
+      log_time = grep.partition("INFO --").first
+      created_at = DateTime.parse(log_time).change(offset:"+0900")
+      code = hash["code"]
+      coupon = {code: code, created_at: created_at}
+      start_time = DateTime.parse "2014-07-27 00:00:00 +0900"
+      finish_time = DateTime.parse "2014-07-27 23:59:59 +0900"
+      if code =~ /[a-z]{5}-\d{4}/ and start_time < created_at and created_at > finish_time
+        coupons << code
+      end
+    end
+    coupons.uniq
+  end
+  
   def send_message
     Message.send_to(self)
   end
   
   def send_retention
-    Message.send_mdm(self)
+    Message.send_mdm_0728(self)
   end
   
   def self.send_retention_message
@@ -20,8 +41,8 @@ class Coupon < ActiveRecord::Base
       offset_start = offset_start + 100
       not_used.where("created_at < ?", DateTime.parse("2014-07-10 23:59:59")).limit(100).offset(offset_start).each do |c|
         unless c.user.nil?
-          # puts c.user.name
-           c.send_retention
+          puts c.user.name
+           # c.send_retention
         end
       end
     end
